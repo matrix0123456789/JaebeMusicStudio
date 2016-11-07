6f914ce83dede8b29e3ccb673a0be61af140aba1
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2016-11-07 21:05:28 +0100

    start

---
+++ JaebeMusicStudio/UI/ButtonPretty.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JaebeMusicStudio.UI
+{
+    /// <summary>
+    /// Interaction logic for Button.xaml
+    /// </summary>
+    public partial class ButtonPretty : UserControl
+    {
+        public ButtonPretty()
+        {
+            InitializeComponent();
+        }
+        [Bindable(true)]
+        public Object Text
+        {
+            get
+            {
+                return button.Content;
+            }
+            set
+            {
+                button.Content = value;
+            }
+        }
+        [Bindable(true)]
+        public string Icon
+        {
+            get
+            {
+                return ((int)(button.Content as string)[0]).ToString("X");
+            }
+            set
+            {
+                button.Content = ((char)int.Parse(value, NumberStyles.AllowHexSpecifier));
+               var a= button.FontFamily.Source;
+            }
+        }
+    }
+}
